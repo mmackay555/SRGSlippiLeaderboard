a46d0a8850b9bedc71966933a8aa2a00387e9bf3
mmackay555 <mmackay555@gmail.com>
2024-11-27 15:39:04 +0000

    Leaderboard v1

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Base application title used in document.title
-  title: 'Colorado Ranked Slippi Leaderboard',
+  title: 'SRG Ranked Slippi Leaderboard',
 
   // use cname option to add CNAME file to webpack build
   // CNAME file allows to use custom domain names with gh-pages, example:
@@ -9,9 +9,9 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'https://grantismo.github.io/CoSlippiLeaderboard/',
+  repoPath: 'https://mmackay555.github.io/SRGSlippiLeaderboard/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
-  spreadsheetID: '1DPIFD0RUA3yjruregmFUbUJ7ccdOjVB2LBp0goHvL-A',
+  spreadsheetID: '19cEJo8O_mahsUH06qhl5pPzXzJDxIt8A-a3IQkX32tM',
 };
